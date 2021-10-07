5db92d8329c01e9799ebb9a6a31093c31467ff27
MatheusFelipeMarinho <matheus_fellip@hotmail.com>
2021-10-07 20:32:51 -0300

    commit init 🚀

---
+++ src/LaravelAddressServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace App;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAddressServiceProvider extends ServiceProvider
+{
+    /**
+     * Method boot
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Method register
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
